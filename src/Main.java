9b3539728867bd747d402ee4dfa22180d5de485e
6564747347456845 <webkonditer@yandex.ru>
2022-04-26 23:24:05 +0400

    Исправлена кодировка

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        //Задание 1
+        //Р—Р°РґР°РЅРёРµ 1
         int i = 1;
         while (i <= 10) {
             System.out.print(i + " ");
@@ -14,13 +14,13 @@ public class Main {
         }
         System.out.print("\n");
 
-        //Задание 2
+        //Р—Р°РґР°РЅРёРµ 2
         int firstFriday = 1;
         for (i=firstFriday; i <= 31; i = i + 7) {
-            System.out.println("Сегодня пятница, "+ i + "-е число. Необходимо подготовить отчет.");
+            System.out.println("РЎРµРіРѕРґРЅСЏ РїСЏС‚РЅРёС†Р°, "+ i + "-Рµ С‡РёСЃР»Рѕ. РќРµРѕР±С…РѕРґРёРјРѕ РїРѕРґРіРѕС‚РѕРІРёС‚СЊ РѕС‚С‡РµС‚.");
         }
 
-        //Задание 3
+        //Р—Р°РґР°РЅРёРµ 3
 
         LocalDate current_date = LocalDate.now();
         int currentYear = current_date.getYear();
@@ -28,7 +28,7 @@ public class Main {
         int stopDate = currentYear + 100;
         for (i=0; i <= stopDate; i = i + 79) {
             if (i >= startDate) {
-                System.out.println( i + " год");
+                System.out.println( i + " РіРѕРґ");
             }
         }
     }
